8af82bff0bcc87a9aa11405201ccb3c66d8d9c52
crouton <68943932+croutonn@users.noreply.github.com>
2021-11-16 00:02:43 +0900

    Update uploader.js

--- uploader.js
+++ uploader.js
@@ -1,20 +1,22 @@
-const core = require('@actions/core');
-const cloudinary = require('cloudinary').v2;
-const path = require('path');
+const core = require("@actions/core");
+const cloudinary = require("cloudinary").v2;
 
 module.exports = function uploader(cloudName, apiKey, apiSecret, files) {
   cloudinary.config({
     cloud_name: cloudName,
     api_key: apiKey,
     api_secret: apiSecret,
-    secure: true
+    secure: true,
   });
 
-  const cloudinaryUploader = file => {
+  const cloudinaryUploader = (file) => {
     core.info(`uploading ${file}`);
 
-    return cloudinary.uploader.upload(file, { public_id: path.basename(file, path.extname(file)) });
+    return cloudinary.uploader.upload(file, {
+      use_filename: true,
+      unique_filename: false,
+    });
   };
 
   return Promise.all(files.map(cloudinaryUploader));
-}
+};
